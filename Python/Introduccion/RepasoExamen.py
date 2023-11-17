60188361ff311a1f16bf4adf76a07c47621c754b
As1ss <alexis.lopbri@educa.jcyl.es>
2023-11-17 15:20:58 +0100

    Ejercicios Listas y Diccionarios repaso

--- Python/Introduccion/RepasoExamen.py
+++ Python/Introduccion/RepasoExamen.py
@@ -1,3 +1,10 @@
+"""
+
+EJERICCIOS BASICOS
+
+
+"""
+
 """Ejercicio 1
 Escribir un programa que muestre por pantalla la cadena ¡Hola Mundo!."""
 
@@ -108,3 +115,229 @@ el interés anual y el número de años, y muestre por pantalla el capital obten
 ##capitalObtenido = cantidadInvertir+((cantidadInvertir *interesAnual) *numAños)
 
 ##print(f"El capital {cantidadInvertir} a interés de {interesAnual} en {numAños} es {capitalObtenido}")
+
+
+"""
+
+EJERCICIOS LISTAS
+
+"""
+
+"""
+Ejercicio 1
+Escribir un programa que almacene las asignaturas de un curso (por ejemplo Matemáticas, Física, Química, Historia y Lengua) en una lista y la muestre por pantalla.
+
+"""
+
+asignaturas = ["Matemáticas","Física","Químca","Historia","Lengua"]
+
+print(asignaturas)
+
+"""
+
+Ejercicio 2
+Escribir un programa que almacene las asignaturas de un curso (por ejemplo Matemáticas, Física, Química, Historia y Lengua) en una lista 
+y la muestre por pantalla el mensaje Yo estudio <asignatura>, donde <asignatura> es cada una de las asignaturas de la lista.
+
+"""
+
+for i in asignaturas:
+    
+    print(f"Yo estudio la asignatura: {i}")
+
+"""
+Ejercicio 3
+Escribir un programa que almacene las asignaturas de un curso (por ejemplo Matemáticas, Física, Química, Historia y Lengua) en una lista, 
+pregunte al usuario la nota que ha sacado en cada asignatura, y después las muestre por pantalla con el mensaje En <asignatura> has sacado <nota> donde <asignatura> 
+es cada una des las asignaturas de la lista y <nota> cada una de las correspondientes notas introducidas por el usuario.
+
+
+notas = []
+for i in asignaturas:
+    nota = int(input(f"Introduce la nota de {i}: "))
+    notas.append(nota)
+
+for i in range(len(asignaturas)):
+    print(f"Has sacado un {notas[i]} en {asignaturas[i]}")
+"""
+
+
+
+
+"""
+Ejercicio 4
+Escribir un programa que pregunte al usuario los números ganadores de la lotería primitiva, 
+los almacene en una lista y los muestre por pantalla ordenados de menor a mayor.
+
+
+
+numerosPremiados = []
+
+for i in range(10):
+    numero=int(input(f"{i+1} Introduce el numero premiado: "))
+    numerosPremiados.append(numero)
+
+numerosPremiados.sort()
+
+for num in numerosPremiados:
+    print(num)
+"""
+
+
+
+"""
+Ejercicio 5
+Escribir un programa que almacene en una lista los números del 1 al 10 y los muestre por pantalla en orden inverso separados por comas.
+
+"""
+
+
+listaNum = []
+for i in range(10):
+    listaNum.append(i+1)
+listaNum.reverse()
+for i in listaNum:
+    print(i,end=",")
+
+print()
+
+"""
+
+Ejercicio 6
+Escribir un programa que almacene las asignaturas de un curso 
+(por ejemplo Matemáticas, Física, Química, Historia y Lengua) en una lista, 
+pregunte al usuario la nota que ha sacado en cada asignatura y elimine de la lista las asignaturas aprobadas. 
+Al final el programa debe mostrar por pantalla las asignaturas que el usuario tiene que repetir.
+
+
+asignaturas = ["Matemáticas","Física","Químca","Historia","Lengua"]
+
+
+asignaturasSuspensas=[]
+
+for i in asignaturas:
+    nota = int(input(f"Introduce la nota de {i} "))
+    if (nota>=5):
+        None
+    else:
+       asignaturasSuspensas.append(i) 
+
+print("Las siguientes asignaturas tienes que repetir")
+for i in asignaturasSuspensas:
+    print(f"Asingatura: {i}")
+
+
+
+
+"""
+
+
+
+
+
+
+"""
+
+EJERCICIOS DICCIONARIOS
+
+"""
+
+"""
+
+Escribir un programa que guarde en una variable el diccionario {'Euro':'€', 'Dollar':'$', 'Yen':'¥'}, 
+pregunte al usuario por una divisa 
+y muestre su símbolo o un mensaje de aviso si la divisa no está en el diccionario.
+
+"""
+
+divisas = {"Euro":"€", "Dollar":"$","Yen":"¥"}
+
+divisa = input("Introduce la divisa para ver el símbolo: " )
+
+
+def devolverSimbolo(moneda):
+    
+    for nombre,simbolo in divisas.items():
+        if (nombre==divisa):
+            return simbolo
+    return "La divisa introducida no está"
+        
+
+
+        
+print(devolverSimbolo(divisa))
+
+
+"""
+Ejercicio 2
+Escribir un programa que pregunte al usuario su nombre, edad, dirección y teléfono y 
+lo guarde en un diccionario. Después debe mostrar por pantalla el mensaje 
+<nombre> tiene <edad> años, vive en <dirección> y su número de teléfono es <teléfono>.
+
+
+
+alumnos = {}
+
+nombre = input(f"Introduce tu nombre: ")
+edad = int(input("Introduce tu edad: "))
+direccion = input("Introduce tu dirección")
+telefono = input("Introduce tu número: ")
+
+
+alumnos[nombre]={"Edad":edad,"Direccion":direccion,"Telefono":telefono}
+
+for i in alumnos:
+    print(i)
+    print(f"Edad: {alumnos[i]['Edad']}")
+    print(f"Direccion: {alumnos[i]['Direccion']}")
+    print(f"Telefono: {alumnos[i]['Telefono']}")
+"""
+
+
+
+
+"""
+
+Ejercicio 3
+Escribir un programa que guarde en un diccionario los precios de las frutas de la 
+tabla, pregunte al usuario por una fruta, un número de kilos y muestre por pantalla
+ el precio de ese número de kilos de fruta. 
+Si la fruta no está en el diccionario debe mostrar un mensaje informando de ello.
+
+
+"""
+
+frutas= {"Pera":0.85,"Platano":1.35,"Manzana":0.80,"Naranja":0.70}
+
+salir = True
+
+while(salir):
+    print("1) Comprar fruta")
+    print("2) Añadir fruta")
+    print("3) Borra fruta")
+    print("4) Salir.")
+    opcion = int(input("Introduce una opción válida: "))
+    print()
+    if(opcion==1):
+        fruta = input("Introduce el nombre de la fruta: ")
+        for i in frutas:
+            if(fruta==i):
+                kilos = int(input("Introduce el peso en kg"))
+        precio = frutas[i] *kilos
+        print(f"El precio de la compra es de : {str(precio)}")
+    if(opcion==2):
+        fruta = input("Introduce el nombre de la fruta: ")
+        precio =float(input("Introduce el precio de la fruta: "))
+        if (fruta in frutas):
+            print("La fruta ya existe compa")
+        else:
+            frutas[fruta] =precio
+            print(frutas)
+    if(opcion==3):
+        fruta =input("Introduce la fruta que quieres borrar")
+        if (fruta in fruta):
+            del frutas[fruta]
+        else:
+            print("Esa fruta no existe compa")
+    if (opcion==4):
+        salir=False
