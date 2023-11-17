b342ccfb0f21e8d3fb35f71e97c802c7fabaa466
As1ss <alexis.lopbri@educa.jcyl.es>
2023-11-17 13:20:20 +0100

    Ejercicios Repaso basico done

---
+++ Python/Introduccion/RepasoExamen.py
@@ -0,0 +1,110 @@
+"""Ejercicio 1
+Escribir un programa que muestre por pantalla la cadena ¡Hola Mundo!."""
+
+print("¡Hola Mundo!.")
+
+
+"""
+
+Ejercicio 2
+Escribir un programa que almacene la cadena ¡Hola Mundo! en una variable y luego muestre por pantalla el contenido de la variable.
+"""
+
+saludo = "¡Hola Mundo!."
+
+print(saludo)
+
+
+"""
+
+Ejercicio 3
+Escribir un programa que pregunte el nombre del usuario en la consola y después de que el usuario lo 
+introduzca muestre por pantalla la cadena ¡Hola <nombre>!, donde <nombre> es el nombre que el usuario haya introducido.
+
+"""
+
+##nombre = input("Introduce tu nombre: ")
+##print(f"¡Hola {nombre}!")
+
+
+"""
+
+Ejercicio 4
+Escribir un programa que muestre por pantalla el resultado de la siguiente operación aritmética 
+ 
+.
+
+"""
+
+operacion = ((3+2)/(2*5))**2
+print(f"Operacion: {str(operacion)}")
+
+
+"""
+Ejercicio 5
+Escribir un programa que pregunte al usuario por el número de horas trabajadas y el coste por hora. Después debe mostrar por pantalla la paga que le corresponde.
+
+"""
+##horasTrabajadas = int(input("Introduce las horas trabajadas: "))
+##costePorHora = int(input("Introduce el coste por hora trabajada: "))
+##print(f"La paga que te corresponde es de: {horasTrabajadas*costePorHora}")
+
+"""
+
+Ejercicio 6
+Escribir un programa que lea un entero positivo, 
+, introducido por el usuario y después muestre en pantalla la suma de todos los enteros desde 1 hasta 
+. La suma de los 
+ primeros enteros positivos puede ser calculada de la siguiente forma:
+
+"""
+
+#num = int(input("Introduce un número: "))
+
+#suma  = num*(num+1)/2
+
+##print(f"Ejercicio 6 suma: {str(suma)}")
+
+"""
+
+Ejercicio 7
+Escribir un programa que pida al usuario su peso (en kg) y estatura (en metros), calcule el índice de masa corporal y lo almacene en una variable, 
+y muestre por pantalla la frase Tu índice de masa corporal es <imc> donde <imc> es el índice de masa corporal calculado redondeado con dos decimales.
+
+"""
+
+#peso = float(input("Introduce tu peso: "))
+#altura = float(input("Introduce tu altura: "))
+
+#imc=peso/(altura**2)
+
+#print(f"Tu imc es: {round(imc,2)}")
+
+"""
+Ejercicio 8
+Escribir un programa que pida al usuario dos números enteros y muestre por pantalla la <n> entre <m> da un cociente <c> y un resto <r> 
+donde <n> y <m> son los números introducidos por el usuario, y <c> y <r> son el cociente y el resto de la división entera respectivamente.
+
+"""
+
+#num1 = int(input("Introduce un número: "))
+#num2 = int(input("Introduce otro número: "))
+#cociente = num1/num2
+#resto = num1%num2
+#print(f"El {num1}/{num2} da de cociente {cociente} y de resto {resto}")
+
+
+"""
+
+Ejercicio 9
+Escribir un programa que pregunte al usuario una cantidad a invertir, 
+el interés anual y el número de años, y muestre por pantalla el capital obtenido en la inversión.
+
+"""
+
+##cantidadInvertir = int(input("Introduce una cantidad a invertir: "))
+##interesAnual = float(input("Introduce el interés anual: "))
+##numAños = int(input("Introduce los años: "))
+##capitalObtenido = cantidadInvertir+((cantidadInvertir *interesAnual) *numAños)
+
+##print(f"El capital {cantidadInvertir} a interés de {interesAnual} en {numAños} es {capitalObtenido}")
